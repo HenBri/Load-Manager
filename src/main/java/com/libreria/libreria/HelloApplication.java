c8d2817c33ce92b8070c17f4d73a76d022b8694e
HenBri <henry.aspeti@gmail.com>
2025-09-02 11:33:17 -0400

    Vista index

--- src/main/java/com/libreria/libreria/HelloApplication.java
+++ src/main/java/com/libreria/libreria/HelloApplication.java
@@ -11,15 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("createBorrower.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 340);
         stage.setTitle("Hello!");
 
         stage.setScene(scene);
         stage.show();
 
-        BorrowerController controller = fxmlLoader.getController();
-        controller.setModo("otro");
+        //BorrowerController controller = fxmlLoader.getController();
+        //controller.setModo("otro");
     }
 
     public static void main(String[] args) {
